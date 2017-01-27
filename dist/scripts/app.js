639ecf26d93d784a5d9d7ece4b0b9115a04b7cd9
khan124 <gooduraiz_khan@yahoo.com>
2017-01-27 13:02:16 -0800

    checkpoint-5

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,6 +9,7 @@
   $stateProvider
          .state('landing', {
              url: '/',
+             controller: 'LandingCtrl as landing',
              templateUrl: '/templates/landing.html'
         })
          
@@ -20,6 +21,7 @@
         $stateProvider
             .state('collection', {
                 url: '/collection',
+            controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
         });
         
