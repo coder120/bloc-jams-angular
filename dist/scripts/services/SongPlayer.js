e493fb6db8f2d13f26e073ab1b265c07725b0e90
khan124 <gooduraiz_khan@yahoo.com>
2017-02-14 18:21:24 -0800

    checkpoint-8-assaignment

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -3,11 +3,21 @@
      function SongPlayer(Fixtures) {
       
         var SongPlayer = {};
-          var currentAlbum = Fixtures.getAlbum();
+         
+        var currentAlbum = Fixtures.getAlbum();
          
     var getSongIndex = function(song) {
      return currentAlbum.songs.indexOf(song);
  };
+         /**
+    * @function playSong
+    * @desc stop audio file and makes song.playing equal to true in order to show pause
+    * @param {Object} song
+    */
+    var stopSong = function(song) {
+      currentBuzzObject.stop();
+      SongPlayer.currentSong.playing = null;
+    };
          
     /**
     * @desc song from album
@@ -30,8 +40,7 @@
     
     var setSong = function(song) {
     if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        currentSong.playing = null;
+         stopSong(song);
     }
  
     currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -80,19 +89,43 @@
      currentBuzzObject.pause();
      song.playing = false;
   }
+  
+  /**
+    * @function SongPlayer.previous
+    * @desc find the previous song in songs' index and plays it
+    */
    SongPlayer.previous = function() {
      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
      currentSongIndex--;
        
        if (currentSongIndex < 0) {
-         currentBuzzObject.stop();
-         SongPlayer.currentSong.playing = null;
+          stopSong(song);
      } else {
          var song = currentAlbum.songs[currentSongIndex];
          setSong(song);
          playSong(song);
      }
  };
+       /**
+    * @function SongPlayer.previous
+    * @desc find the next song in songs' index and plays it
+    */
+         
+         SongPlayer.next = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex++;
+	
+	  if (currentSongIndex > currentAlbum.songs.length - 1) {
+        stopSong(song);
+
+
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    };
+         
   return SongPlayer;
  };
      angular
